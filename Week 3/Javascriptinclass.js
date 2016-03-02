b6376a77f5bfdb41fe9b00884b5d4c21f3023005
Jasmine0112 <Jasminemorrison12@yahoo.com>
2016-03-01 20:30:17 -0800

    first commit

---
+++ Week 3/Javascriptinclass.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>
+    data types, user input, conditional logic
+  </title>
+</head>
+<body>
+  <script type="text/javascript">
+    var name = 'Captain America';
+    var age = 98;
+
+    alert(name);
+    alert(age);
+
+    var do you like sushi = confirm('do you like sushi?');
+    var confirmAdult = prompt('what kind of sushi?');
+
+    alert(doyoulikeentered);
+    alert(confirmtype);      
+  </script>
+</body>
+</html>
---
+++ week1-2/Profile.com
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Test Title <title/>
+</head>
+<body>
+<div id="header>"
+<h1>Jasmine</h1>
+</div>
+<ul>
+<li><a href= "default.asp>Home</a></li>"
+<p> Jasmine Morrison was born in Newark NJ. Her passion is to use tech to advance education. She joined the Rutgers Coding BootCamp in 2016 in the hold to advance her insight in develop!</p>
+</body>
+</>html
